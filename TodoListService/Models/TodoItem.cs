3ccc476ecd61653f0a9f5faf0299b85d804ab81b
Sathiyan R <sathiyan@whiteblue.in>
2023-02-02 10:01:00 +0530

    Add project files.

---
+++ TodoListService/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoListService.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Owner { get; set; }
+    }
+}
